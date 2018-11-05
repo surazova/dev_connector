c0866c4e9f096930c96bd9781047fa2f69f27206
Sitora Urazova <sitoraurazova@gmail.com>
2018-11-05 19:09:47 +0000

    Initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://admin_user:welcome1@ds151393.mlab.com:51393/dev_connector'
+};
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+//Routes
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected')) //success
+    .catch(err => console.log(err)); //errors
+app.get('/', (req,res) => res.send('Hello'));
+
+//Use routes 
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+//Port
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
